a4416469b874e878c8927ae28652c391407f49fb
Valeriy Kuzmin <malcoriel@gmail.com>
2020-12-30 02:40:54 +0100

    feat($srn): restore previous implementation

--- srn/client/src/ThreeLayer/Sphere.tsx
+++ srn/client/src/ThreeLayer/Sphere.tsx
@@ -4,33 +4,22 @@ import { Color, Mesh, MeshBasicMaterial, TextureLoader } from 'three';
 import { CellularNoiseMaterial } from 'threejs-shader-materials';
 import { Material } from 'three/src/materials/Material';
 
-const generalPurposeVertexShader = `
-varying vec2 vUv;
-void main()
-{
-    vUv = uv;
-
-    vec4 mvPosition = modelViewMatrix * vec4(position, 1.0 );
-    gl_Position = projectionMatrix * mvPosition;
-}
-`;
 const noiseMaterial = new CellularNoiseMaterial();
 noiseMaterial.isAnimate = true;
-noiseMaterial.grid = 20;
+noiseMaterial.grid = 75;
 noiseMaterial.divisionScaleX = 2;
 noiseMaterial.speed = 2;
 
 export const Sphere: React.FC<
   MeshProps & { color?: string; star?: boolean }
 > = (props) => {
-  // This reference will give us direct access to the mesh
   const mesh = useRef<Mesh>();
   const space01map = useLoader(TextureLoader, 'resources/space01.jpg');
   const color = props.color || 'white';
 
-  // useFrame(() => {
-  //   if (mesh.current) mesh.current.rotation.y = mesh.current.rotation.y += 0.02;
-  // });
+  useFrame(() => {
+    if (mesh.current) mesh.current.rotation.y = mesh.current.rotation.y += 0.02;
+  });
 
   let starMaterial: Material | undefined;
   if (props.star) {
