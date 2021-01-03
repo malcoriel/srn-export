4c5d8fd574cafc5c02ea6da54f020cd022109524
Valeriy Kuzmin <malcoriel@gmail.com>
2021-01-03 18:04:31 +0100

    feat($srn): focus when clicking on quest items

--- srn/client/src/HtmlLayers/QuestPanel.tsx
+++ srn/client/src/HtmlLayers/QuestPanel.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import NetState, { findMyPlayer } from '../NetState';
-import { QuestState } from '../world';
+import { Planet, Quest, QuestState } from '../world';
 import { findPlanet } from './GameHTMLHudLayer';
 
 export const QuestPanel = () => {
   const ns = NetState.get();
   if (!ns) return null;
-  const { connecting, state, ping, maxPing } = ns;
+  const { state, visualState } = ns;
   const myPlayer = findMyPlayer(state);
-  let questData;
+  let questData:
+    | (Quest & {
+        fromPlanet: Planet;
+        toPlanet: Planet;
+      })
+    | undefined;
   if (myPlayer && myPlayer.quest) {
     const quest = myPlayer.quest;
     const fromPlanet = findPlanet(state, quest.from_id);
@@ -17,8 +22,7 @@ export const QuestPanel = () => {
       questData = {
         fromPlanet,
         toPlanet,
-        reward: quest.reward,
-        state: quest.state,
+        ...quest,
       };
     }
   }
@@ -28,6 +32,11 @@ export const QuestPanel = () => {
   }
   let fromDone = questData.state === QuestState.Picked;
   let toDone = questData.state === QuestState.Delivered;
+
+  const focus = (p: Planet) => {
+    visualState.cameraPosition = { x: p.x, y: p.y };
+    visualState.boundCameraMovement = false;
+  };
   return (
     <div className="panel game-panel quest-panel">
       <div className="header">
@@ -37,11 +46,15 @@ export const QuestPanel = () => {
       <div className="stages">
         <div className={`line ${fromDone ? 'done' : ''}`}>
           1. Pick up the cargo at{' '}
-          <span className="elem">{questData.fromPlanet.name}</span>
+          <span className="elem" onClick={() => focus(questData!.fromPlanet)}>
+            {questData.fromPlanet.name}
+          </span>
         </div>
         <div className={`line ${toDone ? 'done' : ''}`}>
           2. Drop the cargo at{' '}
-          <span className="elem">{questData.toPlanet.name}</span>
+          <span className="elem" onClick={() => focus(questData!.toPlanet)}>
+            {questData.toPlanet.name}
+          </span>
         </div>
       </div>
     </div>
