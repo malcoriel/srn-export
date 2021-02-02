a608eed714655d26f4464eaaa388dc5da4ff08e4
Valeriy Kuzmin <malcoriel@gmail.com>
2021-02-02 23:43:57 +0100

    feat($srn): more universal item grid

--- srn/client/src/HtmlLayers/InventoryWindow.tsx
+++ srn/client/src/HtmlLayers/InventoryWindow.tsx
@@ -13,7 +13,11 @@ const WINDOW_MARGIN = 10;
 const height = (rowCount: number) => CELL_SIZE * rowCount + 1; // 1 is last border
 let MIN_ROWS = 11;
 const WINDOW_HEIGHT = height(MIN_ROWS) + WINDOW_MARGIN * 2;
+const COLUMNS = 11;
+
+const WINDOW_WIDTH = height(COLUMNS) + WINDOW_MARGIN * 2;
 const CONTENT_HEIGHT = 901;  // 15 rows
+const EXTRA_ROWS = 3;
 
 const bounds = {
   left: MARGIN - SCROLL_OFFSET,
@@ -39,7 +43,7 @@ const snap = (pos: IVector): IVector => {
   }
 }
 
-const Item: React.FC<{defaultPosition?:IVector, id: string, position?: IVector, onDrag: (e: any, d: any) => void}> = ({id, position, onDrag, defaultPosition}) => {
+const ItemElem: React.FC<{defaultPosition?:IVector, id: string, position?: IVector, onDrag: (e: any, d: any) => void}> = ({id, position, onDrag, defaultPosition}) => {
   return <Draggable position={position} onStop={onDrag} bounds={bounds} defaultPosition={defaultPosition}>
     <div className='item grabbable-invisible'>
       {id}
@@ -47,38 +51,55 @@ const Item: React.FC<{defaultPosition?:IVector, id: string, position?: IVector, 
   </Draggable>
 }
 
-const EXTRA_ROWS = 3;
-export const InventoryWindow = () => {
-  const [positions, setPositions] = useState<Record<string, IVector>>({
-    "1": {x: 0, y: 0},
-    "2": {x: 1, y: 0},
-    "3": {x: 0, y: 1},
-  });
-  const rowCount = Math.max(((_.max(Object.values(positions).map(p => p.y))|| 0) + 1)  + EXTRA_ROWS , MIN_ROWS);
-  const onDrag = (id: string) => (e: any, d: IVector) => {
+type Item = {
+  id: string
+}
+
+const positionItems = (items: Item[], width: number) : Record<string, IVector> => {
+  let row = 0;
+  let column = 0;
+  let res:Record<string, IVector> = {};
+  for (const item of items) {
+    res[item.id] = new Vector(column, row);
+    column++;
+    if (column >= width) {
+      column = 0;
+      row++;
+    }
+  }
+  return res;
+}
+
+export const ItemGrid: React.FC<{columnCount:number, items: Item[], minRows:number, extraRows: number}> = ({columnCount, items, minRows, extraRows}) => {
+  const [positions, setPositions] = useState<Record<string, IVector>>(positionItems(items, columnCount));
+  const rowCount = Math.max(((_.max(Object.values(positions).map(p => p.y))|| 0) + 1)  + extraRows , minRows);
+  const onDragStop = (id: string) => (e: any, d: IVector) => {
     setPositions((oldPos) => ({
       ...oldPos,
       [id]: positionToGridPosition(d)
     }))
   }
+
+  return <WithScrollbars noAutoHide>
+    <div className="content" style={{height: height(rowCount)}}>
+      <div className='grid' />
+      {items.map(item => <ItemElem key={item.id} id={item.id} position={gridPositionToPosition(positions[item.id])} onDrag={onDragStop(item.id)} />)}
+    </div>
+  </WithScrollbars>
+}
+
+export const InventoryWindow = () => {
+  const items = [{id: "1"}, {id: "2"}, {id: "3"},{id: "4"}, {id: "5"}, {id: "6"}];
   return <Window
     height={WINDOW_HEIGHT}
-    width={681 + SCROLL_OFFSET}
+    width={WINDOW_WIDTH + SCROLL_OFFSET}
     line='complex'
     storeKey='inventoryWindow'
     thickness={8}
     contentClassName="overflow-y-hidden"
   >
     <div className="inventory-window">
-      <WithScrollbars noAutoHide>
-        <div className="content" style={{height: height(rowCount)}}>
-          <div className='grid' />
-          <Item id='1' position={gridPositionToPosition(positions['1'])} onDrag={onDrag('1')} />
-          <Item id='2' position={gridPositionToPosition(positions['2'])} onDrag={onDrag('2')} />
-          <Item id='3' position={gridPositionToPosition(positions['3'])} onDrag={onDrag('3')} />
-        </div>
-      </WithScrollbars>
-
+      <ItemGrid items={items} columnCount={COLUMNS} extraRows={EXTRA_ROWS} minRows={MIN_ROWS}/>
     </div>
   </Window>;
 };
