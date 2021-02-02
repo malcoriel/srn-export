faca09dd5f42a121f588a790c74476e69fb3b430
Valeriy Kuzmin <malcoriel@gmail.com>
2021-02-02 01:16:15 +0100

    feat($srn): better grid

--- srn/client/src/HtmlLayers/InventoryWindow.tsx
+++ srn/client/src/HtmlLayers/InventoryWindow.tsx
@@ -2,6 +2,29 @@ import React, { useEffect } from 'react';
 import './InventoryWindow.scss';
 import { Window } from './ui/Window';
 import Draggable from 'react-draggable';
+import { IVector } from '../utils/Vector';
+
+const bounds = {
+  left: 5,
+  top: 5,
+  right: 661 - 55,
+  bottom: 661 - 55
+}
+
+const cellSize = 60;
+
+const snap = (pos: IVector): IVector => {
+  return {
+    x: Math.floor(pos.x / cellSize) * cellSize,
+    y: pos.y
+  }
+}
+
+const Item: React.FC<{defaultPosition?:IVector}> = ({defaultPosition}) => {
+  return <Draggable bounds={bounds} defaultPosition={defaultPosition}>
+    <div className='item grabbable-invisible' />
+  </Draggable>
+}
 
 export const InventoryWindow = () => {
   return <Window
@@ -13,9 +36,9 @@ export const InventoryWindow = () => {
   >
     <div className="inventory-window">
       <div className="grid"/>
-      <Draggable
-    >
-      <div className='item grabbable-invisible' />
-    </Draggable></div>
+      <Item defaultPosition={{x: 5, y: 5}}/>
+      <Item defaultPosition={{x: 65, y: 5}}/>
+      <Item defaultPosition={{x: 5, y: 65}}/>
+    </div>
   </Window>;
 };
