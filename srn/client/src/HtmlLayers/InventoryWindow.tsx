bb044be82609d2c5a7aca3f49560fa0f5f341f7d
Valeriy Kuzmin <malcoriel@gmail.com>
2021-02-02 01:00:08 +0100

    feat($srn): grid and draggable for the inventory

--- srn/client/src/HtmlLayers/InventoryWindow.tsx
+++ srn/client/src/HtmlLayers/InventoryWindow.tsx
@@ -1,16 +1,21 @@
 import React, { useEffect } from 'react';
 import './InventoryWindow.scss';
 import { Window } from './ui/Window';
-
+import Draggable from 'react-draggable';
 
 export const InventoryWindow = () => {
   return <Window
-    height={800}
-    width={800}
+    height={681}
+    width={681}
     line='complex'
     storeKey='inventoryWindow'
     thickness={8}
   >
-    INVENTORY
+    <div className="inventory-window">
+      <div className="grid"/>
+      <Draggable
+    >
+      <div className='item grabbable-invisible' />
+    </Draggable></div>
   </Window>;
 };
