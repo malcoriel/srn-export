241a4de9c1fcaa0906e411288766054f3861d028
Valeriy Kuzmin <malcoriel@gmail.com>
2021-02-02 12:26:24 +0100

    feat($srn): working but ugly inventory window params

--- srn/client/src/HtmlLayers/InventoryWindow.tsx
+++ srn/client/src/HtmlLayers/InventoryWindow.tsx
@@ -3,27 +3,34 @@ import './InventoryWindow.scss';
 import { Window } from './ui/Window';
 import Draggable from 'react-draggable';
 import Vector, { IVector } from '../utils/Vector';
+import { WithScrollbars } from './ui/WithScrollbars';
 
 const MARGIN = 5;
 const CELL_SIZE = 60;
+const SCROLL_OFFSET = 10;
+const WINDOW_MARGIN = 10;
+const WINDOW_HEIGHT = 681;
+const CONTENT_HEIGHT = 881;
+
+const MAX_Y = CONTENT_HEIGHT;
 
 const bounds = {
-  left: MARGIN,
-  top: MARGIN,
-  right: 661 - (CELL_SIZE - MARGIN),
-  bottom: 661 - (CELL_SIZE - MARGIN)
+  left: MARGIN - SCROLL_OFFSET,
+  top: MARGIN - SCROLL_OFFSET,
+  right: 661 - (CELL_SIZE - MARGIN) - SCROLL_OFFSET,
+  bottom: MAX_Y - (CELL_SIZE - MARGIN) + SCROLL_OFFSET
 }
 
 
 const snap = (pos: IVector): IVector => {
   return {
-    x: Math.round(pos.x / CELL_SIZE) * CELL_SIZE + MARGIN,
-    y: Math.round(pos.y / CELL_SIZE) * CELL_SIZE + MARGIN,
+    x: Math.round(pos.x / CELL_SIZE) * CELL_SIZE + MARGIN - SCROLL_OFFSET,
+    y: Math.round(pos.y / CELL_SIZE) * CELL_SIZE + MARGIN - SCROLL_OFFSET,
   }
 }
 
 const Item: React.FC<{defaultPosition?:IVector, id: string, position?: IVector, onDrag: (e: any, d: any) => void}> = ({id, position, onDrag, defaultPosition}) => {
-  return <Draggable position={position} onDrag={onDrag} bounds={bounds} defaultPosition={defaultPosition}>
+  return <Draggable position={position} onStop={onDrag} bounds={bounds} defaultPosition={defaultPosition}>
     <div className='item grabbable-invisible'>
       {id}
     </div>
@@ -32,9 +39,9 @@ const Item: React.FC<{defaultPosition?:IVector, id: string, position?: IVector, 
 
 export const InventoryWindow = () => {
   const [positions, setPositions] = useState<Record<string, IVector>>({
-    "1": {x: 5, y: 5},
-    "2": {x: 65, y: 5},
-    "3": {x: 5, y: 65},
+    "1": snap({x: 5, y: 5}),
+    "2": snap({x: 65, y: 5}),
+    "3": snap({x: 5, y: 65}),
   });
   const onDrag = (id: string) => (e: any, d: IVector) => {
     setPositions((oldPos) => ({
@@ -43,17 +50,22 @@ export const InventoryWindow = () => {
     }))
   }
   return <Window
-    height={681}
-    width={681}
+    height={WINDOW_HEIGHT}
+    width={681 + SCROLL_OFFSET}
     line='complex'
     storeKey='inventoryWindow'
     thickness={8}
+    contentClassName="overflow-y-hidden"
   >
     <div className="inventory-window">
-      <div className="grid"/>
-      <Item id="1" position={positions["1"]} onDrag={onDrag("1")} />
-      <Item id="2" position={positions["2"]} onDrag={onDrag("2")} />
-      <Item id="3" position={positions["3"]} onDrag={onDrag("3")} />
+      <WithScrollbars noAutoHide>
+        <div className="content">
+          <div className='grid' />
+          <Item id='1' position={positions['1']} onDrag={onDrag('1')} />
+          <Item id='2' position={positions['2']} onDrag={onDrag('2')} />
+          <Item id='3' position={positions['3']} onDrag={onDrag('3')} />
+        </div>
+      </WithScrollbars>
 
     </div>
   </Window>;
--- srn/client/src/HtmlLayers/ui/WithScrollbars.tsx
+++ srn/client/src/HtmlLayers/ui/WithScrollbars.tsx
@@ -2,7 +2,8 @@ import { Scrollbars } from 'rc-scrollbars';
 import React from 'react';
 import "./WithScrollbars.scss";
 
-export const WithScrollbars: React.FC = ({children}) => <Scrollbars
+export const WithScrollbars: React.FC<{noAutoHide?:boolean}> = ({children, noAutoHide}) => <Scrollbars
+  autoHide={!noAutoHide}
   renderThumbHorizontal={(props) => <div {...props} className='ui-thumb' />}
   renderThumbVertical={(props) => <div {...props} className='ui-thumb' />}
   style={{ width: '100%', height: '100%' }}>
