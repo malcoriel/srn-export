35ef46cd046b1635d526e619e2f4f34ababe2d8f
Valeriy Kuzmin <malcoriel@gmail.com>
2021-03-20 00:19:04 +0100

    feat($srn): slightly better arrows

--- srn/client/src/ThreeLayers/ThreeQuestDirection.tsx
+++ srn/client/src/ThreeLayers/ThreeQuestDirection.tsx
@@ -7,6 +7,15 @@ import Vector from '../utils/Vector';
 import { CargoDeliveryQuestState, findPlanet } from '../world';
 import { degToRad } from '../coord';
 import { yellow } from '../utils/palette';
+
+const liftThreePos = (zShift: number) => (
+  threeArrVec: [number, number, number]
+): [number, number, number] => [
+  threeArrVec[0],
+  threeArrVec[1],
+  threeArrVec[2] + zShift,
+];
+
 export const ThreeQuestDirection: React.FC<{
   state: GameState;
   visualState: VisualState;
@@ -24,33 +33,42 @@ export const ThreeQuestDirection: React.FC<{
       : quest.from_id;
   const questTarget = targetId ? findPlanet(state, targetId) : undefined;
   if (!questTarget) return null;
-  let targetPos = Vector.fromIVector(questTarget);
-  targetPos = new Vector(100, 100);
+  const targetPos = Vector.fromIVector(questTarget);
   const normalized = targetPos.subtract(shipPos).normalize();
   const pointerPos = shipPos.add(normalized.scale(10));
-  const leftPoint = pointerPos.add(
-    normalized.scale(-1).turnCounterClockwise(degToRad(45)).scale(2)
-  );
-  const rightPoint = pointerPos.add(
-    normalized.scale(-1).turnCounterClockwise(degToRad(-45)).scale(2)
-  );
-
-  const arrowPoints = [leftPoint, pointerPos, rightPoint];
-  //console.log(arrowPoints.map((v) => v.toFix()));
+  const leftArrowSide = normalized
+    .scale(-1)
+    .turnCounterClockwise(degToRad(60))
+    .scale(2);
+  const rightArrowSide = normalized
+    .scale(-1)
+    .turnCounterClockwise(degToRad(-60))
+    .scale(2);
+  const arrowPoints = [
+    pointerPos.add(leftArrowSide),
+    pointerPos,
+    pointerPos.add(rightArrowSide),
+  ];
+  const smallPointerPos = shipPos.add(normalized.scale(8));
+  const smallArrowPoints = [
+    smallPointerPos.add(leftArrowSide.scale(0.7)),
+    smallPointerPos,
+    smallPointerPos.add(rightArrowSide.scale(0.7)),
+  ];
 
   return (
     <>
       <Line
         flatShading={false}
-        points={[leftPoint, pointerPos].map(vecToThreePos)}
+        points={arrowPoints.map(vecToThreePos).map(liftThreePos(50))}
         color={yellow}
-        lineWidth={5}
+        lineWidth={2}
       />
       <Line
         flatShading={false}
-        points={[rightPoint, pointerPos].map(vecToThreePos)}
+        points={smallArrowPoints.map(vecToThreePos).map(liftThreePos(50))}
         color={yellow}
-        lineWidth={5}
+        lineWidth={2}
       />
     </>
   );
