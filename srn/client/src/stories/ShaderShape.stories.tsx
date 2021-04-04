97ec51590f6015d7b94e9dfaa6ea9e0130302f5b
Valeriy Kuzmin <malcoriel@gmail.com>
2021-04-04 17:17:20 +0200

    feat($srn): add storybook instead of test ui

---
+++ srn/client/src/stories/ShaderShape.stories.tsx
@@ -0,0 +1,44 @@
+import React, { Suspense, useEffect, useState } from 'react';
+import { Story, Meta } from '@storybook/react';
+import { ShaderShape } from '../TestUI/ShaderTestUI';
+import * as uuid from 'uuid';
+import { Canvas } from 'react-three-fiber';
+import { Vector3 } from 'three';
+import { CAMERA_DEFAULT_ZOOM, CAMERA_HEIGHT } from '../ThreeLayers/CameraControls';
+import { size } from '../coord';
+
+
+export default {
+  title: 'Example/ShaderShape',
+  component: ShaderShape
+} as Meta;
+
+const Template: Story = (args) => {
+  const [revision, setRevision] = useState(uuid.v4());
+  useEffect(() => {
+    setRevision((old) => old + 1);
+  }, []);
+  return (
+    <Canvas
+      orthographic
+      camera={{
+        position: new Vector3(0, 0, CAMERA_HEIGHT),
+        zoom: CAMERA_DEFAULT_ZOOM(),
+        far: 1000,
+      }}
+      style={{
+        position: 'absolute'
+      }}
+    >
+        <ambientLight />
+        <pointLight position={[0, 0, CAMERA_HEIGHT]} />
+        <group position={[0, 0, 0]}>
+          <ShaderShape key={revision} {...args} />
+        </group>
+    </Canvas>
+  );
+};
+
+export const Primary = Template.bind({});
+Primary.args = {
+};
