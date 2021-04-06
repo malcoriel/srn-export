0cc7e41749f98567d1110cfa637e22988c87668c
Valeriy Kuzmin <malcoriel@gmail.com>
2021-04-07 01:25:47 +0200

    fix($srn): make the loader less crazy

--- srn/client/src/utils/useResourcesLoading.ts
+++ srn/client/src/utils/useResourcesLoading.ts
@@ -4,6 +4,7 @@ import { useProgress } from '@react-three/drei';
 const expectedResources = [
   // minimal set of resources to load the game
   'resources/ship.stl',
+  'resources/textures/gas-giants/008FA9.png',
 ];
 
 export const useResourcesLoading = () => {
@@ -19,8 +20,8 @@ export const useResourcesLoading = () => {
   }
   const isLoading = Math.abs(threeLoaderProgress - 100) > 1e-9;
   const areLoading = isLoading || missingResources.size > 0;
-  const formattedProgress = `${Math.floor(
-    threeLoaderProgress
+  const formattedProgress = `${Math.floor((loaded / total) * 100).toFixed(
+    0
   )}% (${loaded}/${total})`;
   useEffect(() => {
     if (!areLoading) {
