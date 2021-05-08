335b9cb624f8944efdc0daf93991a83a2f3e3ae4
Valeriy Kuzmin <malcoriel@gmail.com>
2021-05-08 18:17:21 +0200

    fix($srn): properly limit arbitrary movements

--- srn/server/src/locations.rs
+++ srn/server/src/locations.rs
@@ -3,7 +3,7 @@ use crate::new_id;
 use serde_derive::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 use typescript_definitions::{TypescriptDefinition, TypeScriptify};
-use crate::world::{GameState, find_my_ship, find_and_extract_ship, Ship};
+use crate::world::{GameState, find_my_ship, find_and_extract_ship, Ship, find_my_ship_index};
 
 #[derive(Serialize, TypescriptDefinition, TypeScriptify, Deserialize, Debug, Clone)]
 pub struct LocationChangePayload {
@@ -12,11 +12,10 @@ pub struct LocationChangePayload {
 
 
 pub fn try_move_player_ship(state: &mut GameState, player_id: Uuid, location_id: Uuid) -> bool {
-    let ship = find_and_extract_ship(state, player_id);
-
-    if !can_be_moved(state, ship.clone(), location_id) {
+    if !can_be_moved_player(state, player_id, location_id) {
         return false;
     }
+    let ship = find_and_extract_ship(state, player_id);
     let location = state.locations.iter_mut().find(|l| l.id == location_id);
     return if let (Some(location), Some(ship)) = (location, ship) {
         log!(format!("ship {} moved to location {}", ship.id, location_id));
@@ -27,10 +26,12 @@ pub fn try_move_player_ship(state: &mut GameState, player_id: Uuid, location_id:
     }
 }
 
-pub fn can_be_moved(_state: &mut GameState, _ship: Option<Ship>, _location_id: Uuid) -> bool {
-    return true;
-}
-
-pub fn can_be_moved_player(_state: &mut GameState, _player_id: Uuid, _location_id: Uuid) -> bool {
-    return true;
+pub fn can_be_moved_player(state: &mut GameState, player_id: Uuid, _location_id: Uuid) -> bool {
+    let ship_idx = find_my_ship_index(state, player_id);
+    return if let Some(ship_idx) = ship_idx {
+        let loc = &state.locations[ship_idx.location_idx].adjacent_location_ids;
+        loc.iter().any(|l| *l == _location_id)
+    } else {
+        false
+    }
 }
