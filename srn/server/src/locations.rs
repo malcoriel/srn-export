b02ea15a4ec55d3de2ebb7f2b362b4b97495b2e2
Valeriy Kuzmin <malcoriel@gmail.com>
2021-05-09 13:55:01 +0200

    feat($srn): prohibit jumping when docked

--- srn/server/src/locations.rs
+++ srn/server/src/locations.rs
@@ -29,6 +29,9 @@ pub fn try_move_player_ship(state: &mut GameState, player_id: Uuid, location_id:
 pub fn can_be_moved_player(state: &mut GameState, player_id: Uuid, _location_id: Uuid) -> bool {
     let ship_idx = find_my_ship_index(state, player_id);
     return if let Some(ship_idx) = ship_idx {
+        if state.locations[ship_idx.location_idx].ships[ship_idx.ship_idx].docked_at.is_some() {
+            return false;
+        }
         let loc = &state.locations[ship_idx.location_idx].adjacent_location_ids;
         loc.iter().any(|l| *l == _location_id)
     } else {
