debf3f551f4c325fc005bc9bd8d5f7cc6422075b
Valeriy Kuzmin <malcoriel@gmail.com>
2021-05-11 01:36:46 +0200

    feat($srn): make hover hint window support containers

--- srn/client/src/HtmlLayers/HoverHintWindow.tsx
+++ srn/client/src/HtmlLayers/HoverHintWindow.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './HoverHintWindow.scss';
 import { useStore } from '../store';
-import { findMineral } from '../world';
+import { findContainer, findMineral } from '../world';
 import NetState from '../NetState';
 import { useRealToScreen } from '../coordHooks';
 import Vector from '../utils/Vector';
@@ -17,44 +17,71 @@ export const HoverHintWindow: React.FC = () => {
     ? findMineral(ns.state, hintedObjectId)
     : null;
 
+  const hintedContainer = hintedObjectId
+    ? findContainer(ns.state, hintedObjectId)
+    : null;
+
   const { realPosToScreenPos, realLenToScreenLen } = useRealToScreen(ns);
 
   let rendered = null;
-  if (hintedMineral) {
-    const isRare = hintedMineral.value >= 300;
-    const isUncommon = hintedMineral.value >= 200;
-    //const isCommon = hintedMineral.value >= 100;
-    let rarityClass: string;
-    if (isRare) {
-      rarityClass = 'rare';
-    } else {
-      rarityClass = isUncommon ? 'uncommon' : 'common';
-    }
-
-    const coord = realPosToScreenPos(hintedMineral)
-      .add(new Vector(1, 1).scale(realLenToScreenLen(hintedMineral.radius)))
-      .add(WINDOW_OFFSET_PX);
-    rendered = (
-      <div
-        className="hover-hint-window"
-        style={{ top: coord.y, left: coord.x }}
-      >
-        <StyledRect
-          width={150}
-          height={50}
-          line="complex"
-          thickness={2}
-          contentClassName="content"
-        >
-          <div className="hover-hint-window-content">
-            <div className="header">Valuable mineral</div>
-            <div>
-              Rarity: <span className={rarityClass}>{rarityClass}</span>
-            </div>
+  if (hintedMineral || hintedContainer) {
+    let position: Vector | null;
+    let radius: number | null;
+    let windowContent = null;
+    if (hintedMineral) {
+      const isRare = hintedMineral.value >= 300;
+      const isUncommon = hintedMineral.value >= 200;
+      //const isCommon = hintedMineral.value >= 100;
+      let rarityClass: string;
+      if (isRare) {
+        rarityClass = 'rare';
+      } else {
+        rarityClass = isUncommon ? 'uncommon' : 'common';
+      }
+      position = Vector.fromIVector(hintedMineral);
+      radius = hintedMineral.radius;
+      windowContent = (
+        <>
+          <div className="header">Valuable mineral</div>
+          <div>
+            Rarity: <span className={rarityClass}>{rarityClass}</span>
           </div>
-        </StyledRect>
-      </div>
-    );
+        </>
+      );
+    } else if (hintedContainer) {
+      position = Vector.fromIVector(hintedContainer.position);
+      radius = hintedContainer.radius;
+      windowContent = (
+        <>
+          <div className="header">Jettisoned container</div>
+          <div>Maybe there's something valuable inside?</div>
+        </>
+      );
+    } else {
+      position = null;
+      radius = null;
+    }
+    if (position && radius) {
+      const coord = realPosToScreenPos(position)
+        .add(new Vector(1, 1).scale(realLenToScreenLen(radius)))
+        .add(WINDOW_OFFSET_PX);
+      rendered = (
+        <div
+          className="hover-hint-window"
+          style={{ top: coord.y, left: coord.x }}
+        >
+          <StyledRect
+            width={150}
+            height={50}
+            line="complex"
+            thickness={2}
+            contentClassName="content"
+          >
+            <div className="hover-hint-window-content">{windowContent}</div>
+          </StyledRect>
+        </div>
+      );
+    }
   }
 
   return rendered;
