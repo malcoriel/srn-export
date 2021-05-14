e141b39bc08f1b41b0541f41ffe7fa13199906be
Valeriy Kuzmin <malcoriel@gmail.com>
2021-05-15 00:36:44 +0200

    feat($srn): working complex substitution engine for client

--- srn/client/src/utils/substitutions.spec.ts
+++ srn/client/src/utils/substitutions.spec.ts
@@ -3,12 +3,13 @@ global.wasmFunctions = { set_panic_hook: () => {} };
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import {
+  transformAllIntoPrefabs,
   transformLinebreaksStr,
   transformSubstitutionsStr,
 } from './substitutions';
 import { SubstitutionType } from '../world';
 
-describe('transformLinebreaksStr', () => {
+describe('substitution functions', () => {
   it('can insert brs', () => {
     const res = transformLinebreaksStr('qq\nww');
     expect(res).toEqual([
@@ -21,7 +22,38 @@ describe('transformLinebreaksStr', () => {
   });
 
   it('can replace subs', () => {
-    const res = transformSubstitutionsStr('qq s_11 ww s_22', [
+    const res = transformSubstitutionsStr('qq s_1 ww s_2', [
+      {
+        s_type: SubstitutionType.PlanetName,
+        id: '1',
+        text: 'PLANET',
+      },
+      {
+        s_type: SubstitutionType.Generic,
+        id: '2',
+        text: 'GENERIC',
+      },
+    ]);
+    expect(res).toEqual([
+      'qq ',
+      {
+        id: '1',
+        tagName: 'span',
+        tagChildren: 'PLANET',
+        className: 'sub-planet found',
+      },
+      ' ww ',
+      {
+        id: '2',
+        tagName: 'span',
+        tagChildren: 'GENERIC',
+        className: 'sub-generic',
+      },
+    ]);
+  });
+
+  it('can transform both linebreaks and subs', () => {
+    const res = transformAllIntoPrefabs('qq s_1\nww s_2 \n 12s_3', [
       {
         s_type: SubstitutionType.PlanetName,
         id: '1',
@@ -37,15 +69,23 @@ describe('transformLinebreaksStr', () => {
       'qq ',
       {
         tagName: 'span',
+        id: '1',
         tagChildren: 'PLANET',
         className: 'sub-planet found',
       },
-      ' ww ',
+      { tagName: 'br' },
+      'ww ',
       {
         tagName: 'span',
+        id: '2',
         tagChildren: 'GENERIC',
         className: 'sub-generic',
       },
+      ' ',
+      {
+        tagName: 'br',
+      },
+      ' 12s_3',
     ]);
   });
 });
--- srn/client/src/utils/substitutions.tsx
+++ srn/client/src/utils/substitutions.tsx
@@ -5,6 +5,7 @@ import { SubstitutionType } from '../../../world/pkg/world.extra';
 
 type ReactSubPrefab = {
   tagName: string;
+  id: string;
   tagChildren?: React.ReactNode;
   className?: string;
 };
@@ -24,12 +25,17 @@ export const transformLinebreaksStr = (str: string): SubPrefab[] => {
   return newParts;
 };
 
-export const transformLinebreaks = (subs: SubPrefab[]): SubPrefab[] => {
-  return [];
-};
-
-export const transformSubstitutions = (subs: SubPrefab[]): SubPrefab[] => {
-  return [];
+export const transformLinebreaks = (prefabs: SubPrefab[]): SubPrefab[] => {
+  return _.flatMap(prefabs, (p) => {
+    if (typeof p === 'string') {
+      return transformLinebreaksStr(p);
+    }
+    if (_.isArray(p)) {
+      return transformLinebreaks(p);
+    }
+    console.warn('Unsupported transform case for transformLinebreaks', p);
+    return p;
+  }).filter((p) => !!p);
 };
 
 export const enrichSub = (s: Substitution): ReactSubPrefab | null => {
@@ -39,16 +45,19 @@ export const enrichSub = (s: Substitution): ReactSubPrefab | null => {
         tagName: 'span',
         className: 'sub-planet found',
         tagChildren: s.text,
+        id: s.id,
       };
     }
     case SubstitutionType.CharacterName:
       return {
+        id: s.id,
         tagName: 'span',
         className: 'sub-character',
         tagChildren: s.text,
       };
     case SubstitutionType.Generic:
       return {
+        id: s.id,
         tagName: 'span',
         className: 'sub-generic',
         tagChildren: s.text,
@@ -57,35 +66,90 @@ export const enrichSub = (s: Substitution): ReactSubPrefab | null => {
     default: {
       console.warn(`Unknown substitution ${s.s_type} text ${s.text}`);
       return {
+        id: s.id,
         tagName: 'span',
         tagChildren: s.text,
       };
     }
   }
 };
+
+const SUB_RE = /s_(\w+)/;
+
 export const transformSubstitutionsStr = (
   text: string,
   subs: Substitution[]
 ): SubPrefab[] => {
-  const parts = text.split(/s_\w+/);
-  const substitutions = subs.map((s, i) => {
-    return enrichSub(s);
-  });
-  return _.flatMap(_.zip(parts, substitutions)).filter(
-    (p) => !!p
-  ) as SubPrefab[];
+  const subsById = _.keyBy(
+    subs.map((s, i) => {
+      return enrichSub(s);
+    }),
+    'id'
+  );
+
+  const result = [];
+  for (let i = 0; i < text.length; i++) {
+    const remaining = text.slice(i);
+    // console.log(`remaining '${remaining}'`);
+    const match = remaining.match(SUB_RE);
+    if (match) {
+      const matchedStr = match[0];
+      // console.log('match at', i, matchedStr);
+      const index = match.index;
+      if (!index) {
+        console.warn('match with no index', match);
+        break;
+      }
+      result.push(text.slice(i, i + index));
+      const subId = match[1] || 'unknown';
+      const matchingSub = subsById[subId];
+      if (matchingSub) {
+        result.push(matchingSub);
+        i = i + index + matchedStr.length - 1;
+        // console.log('sub found', index, matchedStr.length);
+        // console.log('jump to', i);
+      } else {
+        if (_.isString(result[result.length - 1])) {
+          result[result.length - 1] += matchedStr;
+        } else {
+          // console.warn(`sub not found id ${subId}`, index, matchedStr.length);
+          result.push(matchedStr);
+        }
+        i = i + index + matchedStr.length - 1;
+        // console.log('jump to', i);
+      }
+    } else {
+      result.push(remaining);
+      break;
+    }
+  }
+  return result;
 };
 
-export const preprocessSubstitutedText = (
-  text: string,
+export const transformSubstitutions = (
+  prefabs: SubPrefab[],
   subs: Substitution[]
 ): SubPrefab[] => {
-  return [];
+  return _.flatMap(prefabs, (p) => {
+    if (_.isString(p)) {
+      return transformSubstitutionsStr(p, subs);
+    }
+    if (_.isArray(p)) {
+      return transformSubstitutions(p, subs);
+    }
+    if (p.tagName === 'br') {
+      return p;
+    }
+    console.warn('Unsupported transform case for transformSubstitutions', p);
+    return p;
+  });
 };
 
-export const reactifySubstitutedText = (
-  subs: SubPrefab[]
-): React.ReactNode[] => {
+export const preprocessSubstitutedText = (text: string): SubPrefab[] => {
+  return [text];
+};
+
+export const reactifyPrefabs = (subs: SubPrefab[]): React.ReactNode[] => {
   const res = [];
   for (let i = 0; i < subs.length; i++) {
     const sub = subs[i];
@@ -93,5 +157,15 @@ export const reactifySubstitutedText = (
       res.push(<span key={i}>{sub}</span>);
     }
   }
+  return res;
+};
+
+export const transformAllIntoPrefabs = (
+  str: string,
+  subs: Substitution[]
+): SubPrefab[] => {
+  let res = preprocessSubstitutedText(str);
+  res = transformLinebreaks(res);
+  res = transformSubstitutions(res, subs);
   return res;
 };
