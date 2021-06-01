56fe35decab55329fb1e34211d21d41b99c3ccaf
Valeriy Kuzmin <malcoriel@gmail.com>
2021-06-01 23:45:35 +0200

    fix($srn): adjust menu positioning

--- srn/client/src/HtmlLayers/GlobalContextMenu.tsx
+++ srn/client/src/HtmlLayers/GlobalContextMenu.tsx
@@ -1,7 +1,7 @@
 import { useStore } from '../store';
 import { ControlledMenu, MenuItem } from '@szhsin/react-menu';
 import _ from 'lodash';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './GlobalContextMenu.scss';
 
 export interface GlobalContextMenuItem {
@@ -10,16 +10,28 @@ export interface GlobalContextMenuItem {
   onClick: () => void;
 }
 
+const REPOSITION_INTERVAL = 100;
 export const GlobalContextMenu = () => {
   const menuAnchorRef = useStore((state) => state.contextMenuRef);
   const isMenuOpen = useStore((state) => state.contextMenuItems.length > 0);
   const items = useStore((state) => state.contextMenuItems);
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    const timer = setInterval(
+      () => setCounter((c: number) => c + 1),
+      REPOSITION_INTERVAL
+    );
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
   return (
     <ControlledMenu
       animation={false}
       anchorRef={menuAnchorRef}
       isOpen={isMenuOpen}
       className="global-context-menu"
+      repositionFlag={counter}
     >
       {items.map((item, i) => (
         <MenuItem
