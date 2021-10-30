837c1b58193fa965a450e993df7d29fcc7677fc5
Valeriy Kuzmin <malcoriel@gmail.com>
2021-10-31 01:26:33 +0200

    feat($srn): continue test implementation, add finder and invariant

--- srn/client/src/ThreeLayers/blocks/ThreeInteractor.spec.tsx
+++ srn/client/src/ThreeLayers/blocks/ThreeInteractor.spec.tsx
@@ -7,8 +7,38 @@ import { store } from '../../store';
 import * as util from 'util';
 import { ObjectSpecifierBuilder } from '../../../../world/pkg/world.extra';
 
+// must stay function due to ts quirk
+export function invariant(condition: any, message?: string): asserts condition {
+  if (!condition) {
+    throw new Error(message);
+  }
+}
+
+const findNested = (
+  obj: Record<string, any>,
+  keyToFind: string,
+  valueToFind: string | RegExp
+): any[] =>
+  Object.entries(obj).reduce((acc: string[], [key, value]) => {
+    if (key === keyToFind) {
+      let matched = false;
+      if (valueToFind instanceof RegExp) {
+        matched = valueToFind.test(value);
+      } else {
+        matched = valueToFind === value;
+      }
+      if (matched) {
+        return acc.concat(obj as any);
+      }
+    }
+    if (typeof value === 'object') {
+      return acc.concat(findNested(value, keyToFind, valueToFind));
+    }
+    return acc;
+  }, []);
+
 describe('ThreeInteractor', () => {
-  it('can render with mock store', async () => {
+  it('can render and change state with mock store', async () => {
     const renderer = await ReactThreeTestRenderer.create(
       <StoryCanvasInternals>
         <ThreeInteractor
@@ -27,6 +57,12 @@ describe('ThreeInteractor', () => {
         }),
       });
     });
-    console.log(util.inspect(renderer.toGraph(), false, 8));
+    const graph = renderer.toGraph();
+    const tree = renderer.toTree();
+    invariant(graph && tree);
+    console.log(util.inspect(graph, false, 8));
+    console.log({
+      found: findNested(tree, 'name', /main/),
+    });
   });
 });
