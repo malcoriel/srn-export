a24da43f037d6eba26fe5cbbd98674c725fe88ae
Valeriy Kuzmin <malcoriel@gmail.com>
2022-02-27 00:47:35 +0100

    fix($srn): build script

--- srn/scripts/deploy.js
+++ srn/scripts/deploy.js
@@ -55,7 +55,7 @@ const doBuildServer = async () => {
         -v cargo-git:/home/rust/.cargo/git \
         -v cargo-registry:/home/rust/.cargo/registry \
         -v target:/home/rust/src/target \
-        ekidd/rust-musl-builder:nightly-2021-02-13 \
+        ekidd/rust-musl-builder:nightly-2021-12-23 \
         /bin/bash -c "sudo chown -R rust:rust /home/rust/.cargo/git /home/rust/.cargo/registry /home/rust/src/target; cargo build --release;"\
         `
   );
