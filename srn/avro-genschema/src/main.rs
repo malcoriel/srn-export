c8efe872ed1be7744cac9c07cd3fe605f7e2e645
Valeriy Kuzmin <malcoriel@gmail.com>
2022-12-28 20:05:11 +0100

    feat($srn): play with the avro genschema

--- srn/avro-genschema/src/main.rs
+++ srn/avro-genschema/src/main.rs
@@ -1,5 +1,50 @@
 use clap::command;
+use std::collections::HashMap;
 use std::fs;
+use syn::visit::Visit;
+use syn::{visit, Field, ItemFn};
+use uuid::*;
+
+struct FnVisitor;
+
+pub struct SpatialProps {
+    pub foo: i32,
+    pub bar: String,
+}
+
+pub enum Movement {
+    Alpha(i32),
+    Beta { baz: f64 },
+}
+
+pub struct ObjectProperty {
+    pub empty: f32,
+}
+
+pub struct PlanetV2 {
+    pub id: Uuid,
+    pub name: String,
+    pub spatial: SpatialProps,
+    pub movement: Movement,
+    pub anchor_tier: u32,
+    pub color: String,
+    pub health: Option<ObjectProperty>,
+    pub properties: Vec<ObjectProperty>,
+    pub properties2: HashMap<String, ObjectProperty>,
+}
+
+impl<'ast> Visit<'ast> for FnVisitor {
+    fn visit_field(&mut self, node: &'ast Field) {
+        println!(
+            "Field with name={}",
+            node.ident
+                .as_ref()
+                .map(|i| i.to_string())
+                .unwrap_or("unnamed".to_string())
+        );
+        visit::visit_field(self, node);
+    }
+}
 
 fn main() {
     let cmd = clap::Command::new("avro-genschema")
@@ -23,12 +68,18 @@ fn main() {
     };
     let from = matches.get_one::<std::path::PathBuf>("from");
     let to = matches.get_one::<std::path::PathBuf>("to");
-    if let Some(from) = from {
+    let file = if let Some(from) = from {
         if let Some(to) = to {
             let src = fs::read_to_string(from).expect("file not found");
             let ast = syn::parse_file(src.as_str())
                 .expect(format!("Unable to create AST from file {:?}", from).as_str());
-            println!("ast: {:?}", ast);
+            Some(ast)
+        } else {
+            None
         }
+    } else {
+        None
     }
+    .expect("could not parse file");
+    FnVisitor.visit_file(&file)
 }
