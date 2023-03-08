b14859db5c66b07558d4efe76f699c972a69e591
Valeriy Kuzmin <malcoriel@gmail.com>
2023-03-08 21:50:56 +0100

    feat($srn): more or less finish single item trajectory story

--- srn/client/src/ThreeLayers/ThreeTrajectory.stories.tsx
+++ srn/client/src/ThreeLayers/ThreeTrajectory.stories.tsx
@@ -3,15 +3,19 @@ import { Meta, Story } from '@storybook/react';
 import * as uuid from 'uuid';
 import { StoryCanvas } from '../TestUI/StoryCanvas';
 import { ThreeSpaceBackground } from './ThreeSpaceBackground';
-import Vector, { IVector, VectorF } from '../utils/Vector';
+import Vector, {
+  getCounterClockwiseAngleMath,
+  IVector,
+  VectorF,
+} from '../utils/Vector';
 import { posToThreePos } from './util';
 import Color from 'color';
 import _ from 'lodash';
 
 export interface ThreeTrajectoryItemProps {
   position: IVector;
-  velocityNormalized: IVector; // len 0.0 - no speed, 1.0 - maximal speed, direction = speed direciton
-  accNormalized: IVector; // len 0.0 - no acc, 1.0 - maximal acceleration
+  velocityNormalized: Vector; // len 0.0 - no speed, 1.0 - maximal speed, direction = speed direciton
+  accNormalized: Vector; // len 0.0 - no acc, 1.0 - maximal acceleration
   mainColor: string;
   accColor: string;
 }
@@ -25,8 +29,17 @@ const ThreeTrajectoryItem: React.FC<ThreeTrajectoryItemProps> = ({
   velocityNormalized,
   accNormalized,
 }) => {
-  const angleRad = Math.PI / 4;
-  const angleRad2 = Math.PI / 2;
+  const showAcc = Math.abs(accNormalized.length()) > 1e-6;
+  const showVel = Math.abs(velocityNormalized.length()) > 1e-6;
+  const [angleVel, angleAcc] = useMemo(() => {
+    const vel = showVel
+      ? getCounterClockwiseAngleMath(VectorF(1, 0), velocityNormalized)
+      : 0;
+    const acc = showAcc
+      ? getCounterClockwiseAngleMath(VectorF(1, 0), accNormalized)
+      : 0;
+    return [vel - Math.PI / 2, acc - Math.PI / 2];
+  }, [accNormalized, velocityNormalized]);
   const [minColorL, maxColorL, baseColor] = useMemo(() => {
     const base = new Color(mainColor);
     return [base.darken(0.5).lightness(), base.lighten(0.5).lightness(), base];
@@ -37,10 +50,9 @@ const ThreeTrajectoryItem: React.FC<ThreeTrajectoryItemProps> = ({
     const lerped = lerp(minColorL, maxColorL, len);
     return _.cloneDeep(baseColor).lightness(lerped);
   }, [minColorL, maxColorL, baseColor, velocityNormalized]);
-  console.log({ color });
   return (
     <group position={posToThreePos(position.x, position.y)}>
-      <group rotation={[0, 0, angleRad]}>
+      <mesh rotation={[0, 0, angleVel]}>
         <mesh>
           <circleBufferGeometry args={[3, 16]} />
           <meshBasicMaterial
@@ -49,25 +61,29 @@ const ThreeTrajectoryItem: React.FC<ThreeTrajectoryItemProps> = ({
             color={color.hex().toString()}
           />
         </mesh>
-        <mesh
-          rotation={[Math.PI * (1.66 + 0.66), Math.PI, Math.PI * 1.25]}
-          position={[0, 2.7, -2]}
-          scale={[1.5, 1.5, 1.0]}
-        >
-          <tetrahedronGeometry args={[2, 0]} />
-          <meshBasicMaterial opacity={1.0} color={color.hex().toString()} />
+        {showVel && (
+          <mesh
+            rotation={[Math.PI * (1.66 + 0.66), Math.PI, Math.PI * 1.25]}
+            position={[0, 2.7, -2]}
+            scale={[1.5, 1.5, 1.0]}
+          >
+            <tetrahedronGeometry args={[2, 0]} />
+            <meshBasicMaterial opacity={1.0} color={color.hex().toString()} />
+          </mesh>
+        )}
+      </mesh>
+      {showAcc && (
+        <mesh rotation={[0, 0, angleAcc]}>
+          <mesh
+            rotation={[Math.PI * (1.66 + 0.66), Math.PI, Math.PI * 1.25]}
+            position={[0, 3.0, -1]}
+            scale={[0.75, 0.75, 0.5]}
+          >
+            <tetrahedronGeometry args={[2, 0]} />
+            <meshBasicMaterial opacity={1.0} color={accColor} />
+          </mesh>
         </mesh>
-      </group>
-      <group rotation={[0, 0, angleRad2]}>
-        <mesh
-          rotation={[Math.PI * (1.66 + 0.66), Math.PI, Math.PI * 1.25]}
-          position={[0, 3.0, -1]}
-          scale={[0.75, 0.75, 0.5]}
-        >
-          <tetrahedronGeometry args={[2, 0]} />
-          <meshBasicMaterial opacity={1.0} color={accColor} />
-        </mesh>
-      </group>
+      )}
     </group>
   );
 };
@@ -78,13 +94,14 @@ const Template: Story = (args) => {
     setRevision((old) => old + 1);
   }, []);
   return (
-    <div key={`${revision}+${JSON.stringify(args)}`}>
+    <div>
       <StoryCanvas zoom={16.0}>
         <ThreeSpaceBackground size={256} shaderShift={0} />
         <ThreeTrajectoryItem
+          key={`${revision}+${JSON.stringify(args)}`}
           position={VectorF(0, 0)}
-          velocityNormalized={VectorF(1.0, 1.0)}
-          accNormalized={VectorF(0, 1)}
+          velocityNormalized={VectorF(args.velX, args.velY)}
+          accNormalized={VectorF(args.accX, args.accY)}
           mainColor="red"
           accColor="green"
         />
@@ -95,11 +112,28 @@ const Template: Story = (args) => {
 
 export const SingleItem = Template.bind({});
 SingleItem.args = {
-  tickInterval: 50,
+  velX: 0.7,
+  velY: 1,
+  accX: 0,
+  accY: 1,
 };
+
 // noinspection JSUnusedGlobalSymbols
+const oneToOne = {
+  control: {
+    type: 'range',
+    min: -1.0,
+    max: 1.0,
+    step: 0.1,
+  },
+};
 export default {
   title: 'Three/Trajectory',
   component: ThreeTrajectoryItem,
-  argTypes: {},
+  argTypes: {
+    velX: oneToOne,
+    velY: oneToOne,
+    accX: oneToOne,
+    accY: oneToOne,
+  },
 } as Meta;
