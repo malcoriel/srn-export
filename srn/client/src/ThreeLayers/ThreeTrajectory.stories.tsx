4ce0ebaf8d972fc35e2d2852b8ff8766275ea93b
Valeriy Kuzmin <malcoriel@gmail.com>
2023-03-08 21:31:25 +0100

    feat($srn): continue implementing trajectory item

--- srn/client/src/ThreeLayers/ThreeTrajectory.stories.tsx
+++ srn/client/src/ThreeLayers/ThreeTrajectory.stories.tsx
@@ -1,33 +1,73 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Meta, Story } from '@storybook/react';
 import * as uuid from 'uuid';
 import { StoryCanvas } from '../TestUI/StoryCanvas';
 import { ThreeSpaceBackground } from './ThreeSpaceBackground';
-import { IVector, VectorF } from '../utils/Vector';
+import Vector, { IVector, VectorF } from '../utils/Vector';
 import { posToThreePos } from './util';
+import Color from 'color';
+import _ from 'lodash';
 
 export interface ThreeTrajectoryItemProps {
   position: IVector;
-  speedNormalized: IVector; // 0.0 - no speed, 1.0 - maximal speed, direction = speed direciton
+  velocityNormalized: IVector; // len 0.0 - no speed, 1.0 - maximal speed, direction = speed direciton
+  accNormalized: IVector; // len 0.0 - no acc, 1.0 - maximal acceleration
+  mainColor: string;
+  accColor: string;
 }
+
+const lerp = (a: number, b: number, v: number) => a + (b - a) * v;
 
 const ThreeTrajectoryItem: React.FC<ThreeTrajectoryItemProps> = ({
   position,
+  mainColor,
+  accColor,
+  velocityNormalized,
+  accNormalized,
 }) => {
+  const angleRad = Math.PI / 4;
+  const angleRad2 = Math.PI / 2;
+  const [minColorL, maxColorL, baseColor] = useMemo(() => {
+    const base = new Color(mainColor);
+    return [base.darken(0.5).lightness(), base.lighten(0.5).lightness(), base];
+  }, [mainColor]);
+  const color = useMemo(() => {
+    let len = Vector.fromIVector(velocityNormalized).length();
+    len = Math.max(0.0, Math.min(1.0, len));
+    const lerped = lerp(minColorL, maxColorL, len);
+    return _.cloneDeep(baseColor).lightness(lerped);
+  }, [minColorL, maxColorL, baseColor, velocityNormalized]);
+  console.log({ color });
   return (
     <group position={posToThreePos(position.x, position.y)}>
-      <mesh>
-        <circleBufferGeometry args={[3, 16]} />
-        <meshBasicMaterial opacity={1.0} transparent color="red" />
-      </mesh>
-      <mesh
-        rotation={[Math.PI * (1.66 + 0.66), Math.PI, Math.PI * 1.25]}
-        position={[0, 2.7, -1]}
-        scale={[1.5, 1.5, 1.0]}
-      >
-        <tetrahedronGeometry args={[2, 0]} />
-        <meshBasicMaterial opacity={1.0} color="yellow" />
-      </mesh>
+      <group rotation={[0, 0, angleRad]}>
+        <mesh>
+          <circleBufferGeometry args={[3, 16]} />
+          <meshBasicMaterial
+            opacity={1.0}
+            transparent
+            color={color.hex().toString()}
+          />
+        </mesh>
+        <mesh
+          rotation={[Math.PI * (1.66 + 0.66), Math.PI, Math.PI * 1.25]}
+          position={[0, 2.7, -2]}
+          scale={[1.5, 1.5, 1.0]}
+        >
+          <tetrahedronGeometry args={[2, 0]} />
+          <meshBasicMaterial opacity={1.0} color={color.hex().toString()} />
+        </mesh>
+      </group>
+      <group rotation={[0, 0, angleRad2]}>
+        <mesh
+          rotation={[Math.PI * (1.66 + 0.66), Math.PI, Math.PI * 1.25]}
+          position={[0, 3.0, -1]}
+          scale={[0.75, 0.75, 0.5]}
+        >
+          <tetrahedronGeometry args={[2, 0]} />
+          <meshBasicMaterial opacity={1.0} color={accColor} />
+        </mesh>
+      </group>
     </group>
   );
 };
@@ -41,7 +81,13 @@ const Template: Story = (args) => {
     <div key={`${revision}+${JSON.stringify(args)}`}>
       <StoryCanvas zoom={16.0}>
         <ThreeSpaceBackground size={256} shaderShift={0} />
-        <ThreeTrajectoryItem position={VectorF(0, 0)} />
+        <ThreeTrajectoryItem
+          position={VectorF(0, 0)}
+          velocityNormalized={VectorF(1.0, 1.0)}
+          accNormalized={VectorF(0, 1)}
+          mainColor="red"
+          accColor="green"
+        />
       </StoryCanvas>
     </div>
   );
@@ -54,5 +100,6 @@ SingleItem.args = {
 // noinspection JSUnusedGlobalSymbols
 export default {
   title: 'Three/Trajectory',
+  component: ThreeTrajectoryItem,
   argTypes: {},
 } as Meta;
